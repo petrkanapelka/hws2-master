f1612f243f5ea05c4e157fe4255bf8a1a1635569
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-23 09:12:25 +0300

    ffeat: add types to Message

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import s from './Message.module.css'
+import { MessageType } from '../HW1'
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+    message: MessageType
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
@@ -11,18 +14,22 @@ const Message = (props: MessagePropsType) => {
             <div className={s.imageAndText}>
                 <img
                     id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
+                    alt='avatar'
+                    src={props.message.user.avatar}
+                // создаёт студент
 
-                    //
+                //
                 />
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
+                        {props.message.user.name}
 
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
+                        {props.message.message.text}
 
                         {/**/}
                     </pre>
@@ -30,6 +37,7 @@ const Message = (props: MessagePropsType) => {
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
+                {props.message.message.time}
 
                 {/**/}
             </div>
