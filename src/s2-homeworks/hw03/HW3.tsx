ccf37267423ec664f95df400453582b2f8807cb6
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-04 22:03:18 +0300

    feat: add hr to HW3

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -41,6 +41,7 @@ export const HW3 = () => {
     return (
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #3</div>
+            <hr></hr>
             {/*для автоматической проверки дз (не менять)*/}
 
             <div className={s2.hw}>
