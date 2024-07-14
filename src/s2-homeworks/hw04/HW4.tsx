e871ca0f47e45f5f61e21d7fba8676f0d935a5c5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-14 22:33:04 +0300

    feat: update HW4

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -13,7 +13,7 @@ const HW4 = () => {
     return (
         <div id={'hw4'}>
             <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
+            {/* демонстрация возможностей компонент: */}
             <div className={s2.hw}>
                 <Stand />
             </div>
