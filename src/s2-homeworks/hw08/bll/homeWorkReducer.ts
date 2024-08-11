16e62463085504bbd1e1f4f930060fb3297516a7
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-11 22:59:58 +0300

    refactor: homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -3,21 +3,19 @@ import { UserType } from '../HW8';
 type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
-    // need to fix any
     switch (action.type) {
         case 'sort': {
-            // by name
-
             return [
                 ...state.sort((a, b) => {
                     return action.payload === 'up'
                         ? a.name.localeCompare(b.name)
                         : b.name.localeCompare(a.name);
                 }),
-            ]; // need to fix
+            ];
         }
         case 'check': {
-            return state.filter((user) => user.age >= action.payload); // need to fix
+            const filteredState = state.filter((user) => user.age >= action.payload);
+            return filteredState.sort((a, b) => a.name.localeCompare(b.name));
         }
         default:
             return state;
