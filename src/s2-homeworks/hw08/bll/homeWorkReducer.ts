6aab8581955fed37e4869f0ed22e27b84fbd9070
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-11 22:32:07 +0300

    feat: homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,25 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+    // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            // by name
 
-            return state // need to fix
+            return [
+                ...state.sort((a, b) => {
+                    return action.payload === 'up'
+                        ? a.name.localeCompare(b.name)
+                        : b.name.localeCompare(a.name);
+                }),
+            ]; // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((user) => user.age >= 18); // need to fix
         }
         default:
-            return state
+            return state;
     }
-}
+};
