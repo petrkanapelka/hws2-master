70ff6439ba12727a5265d12282f02b49f906b7be
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-18 06:59:51 +0300

    feat: update Clock

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -30,7 +30,6 @@ function Clock() {
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
         setShow(false)
     }
-    console.log(date)
 
     const stringTime = date.toLocaleTimeString('en-GB', { hour12: false }) || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = new Intl.DateTimeFormat().format() || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
