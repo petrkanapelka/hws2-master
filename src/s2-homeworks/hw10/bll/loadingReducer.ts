de8624c2d2f526bdede6590f370dab233f2f4117
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-27 22:12:37 +0300

    feat: update loadingReducers

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,32 @@
 const initState = {
     isLoading: false,
-}
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type LoadingType = {
+    isLoading: boolean;
+};
+
+export type ActionType = ReturnType<typeof loadingAC>;
+
+export const loadingReducer = (state = initState, action: ActionType): LoadingType => {
+    // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+            return { ...state, isLoading: action.isLoading };
+        }
 
         default:
-            return state
+            return state;
     }
-}
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+    type: 'CHANGE_LOADING';
+    isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
-})
+});
