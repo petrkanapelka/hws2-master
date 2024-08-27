e7f5b496a2b7e329eb577b5bddf2d77cc8b7fbd7
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-27 22:12:21 +0300

    feat: update Loader

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,8 @@
 import s from './Loader.module.css'
+// import { ReactComponent as Icon } from './icon.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <div className={s.loader}>
+        {/* <Icon /> */}
+    </div>
+)
