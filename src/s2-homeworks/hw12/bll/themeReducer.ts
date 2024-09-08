9581205f5bf28b6fce45c27d9cc8895b149cabac
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-09-08 09:19:58 +0300

    feat: update themeReducer

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
 const initState = {
     themeId: 1,
-}
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type StateType = typeof initState;
+
+export const themeReducer = (state = initState, action: ActionType): StateType => {
+    // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID': {
+            return { ...state, themeId: action.id };
+        }
         default:
-            return state
+            return state;
     }
-}
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = ChangeThemeType;
+
+type ChangeThemeType = {
+    type: string;
+    id: number;
+};
+
+export const changeThemeId = (id: number): ChangeThemeType =>
+    ({ type: 'SET_THEME_ID', id } as const); // fix any
