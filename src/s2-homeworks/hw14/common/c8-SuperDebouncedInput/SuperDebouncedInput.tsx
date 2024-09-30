16dc37da555174a81400a7a6f814cddb241b323d
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-09-30 19:31:01 +0300

    Fix: Implement debouncing in SuperDebouncedInput
    Introduces a debounce mechanism to the `SuperDebouncedInput` component's `onChangeText` handler. This prevents excessive firing of the `onDebouncedChange` callback, optimizing performance by ensuring it's only called after a 1500ms delay from the last input change.

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -1,4 +1,4 @@
-import React, {DetailedHTMLProps, InputHTMLAttributes, ReactNode, useState} from 'react'
+import React, { DetailedHTMLProps, InputHTMLAttributes, ReactNode, useState } from 'react'
 import SuperInputText from '../../../hw04/common/c1-SuperInputText/SuperInputText'
 
 // тип пропсов обычного инпута
@@ -15,8 +15,8 @@ export type SuperDebouncedInputPropsType = Omit<DefaultInputPropsType, 'type'> &
     spanClassName?: string
 } // илм экспортировать тип SuperInputTextPropsType
     & { // плюс специальный пропс SuperPagination
-    onDebouncedChange?: (value: string) => void
-}
+        onDebouncedChange?: (value: string) => void
+    }
 
 const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
     {
@@ -33,16 +33,21 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
-
+            if(timerId) {
+                clearTimeout(timerId)
+            }
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
-
+            const newTimerId = window.setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500);
+            setTimerId(newTimerId)
             //
         }
     }
 
     return (
-        <SuperInputText onChangeText={onChangeTextCallback} {...restProps}/>
+        <SuperInputText onChangeText={onChangeTextCallback} {...restProps} />
     )
 }
 
